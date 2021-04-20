0a5cb677aa1445d17dcbfbc5a009612bbc941e81
Tdear <wish1076@naver.com>
2021-04-20 23:57:44 +0900

    JPA NamedQuery - 엔테티에서 메서드명으로 namedQuery 를 찾는다. 따라서 없어도됨 - 실무에서 거의 사용안함 -> 엔티티에 쿼리 쓰는게 이상함 , 다른 기능이 대체함 - 가장 큰 장점 쿼리가 잘못된경우, application 로딩 시점에 오류를 감지

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -8,6 +8,10 @@ import javax.persistence.*;
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id","username","age"}) // 연관관계는 생략
+@NamedQuery(
+        name="Member.findByUsername"
+        , query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
@@ -8,4 +10,10 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // @Query(name="Member.findByUsername")
+    // 엔테티에서 메서드명으로 namedQuery 를 찾는다. 따라서 없어도됨
+    // 실무에서 거의 사용안함 -> 엔티티에 쿼리 쓰는게 이상함 , 다른 기능이 대체함
+    // 가장 큰 장점 쿼리가 잘못된경우, application 로딩 시점에 오류를 감지
+    List<Member> findByUsername(@Param("username") String username);
 }
