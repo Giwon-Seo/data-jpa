ae96fb7970af779f7cb5bddcc35e82d4bf0c8ff7
Tdear <wish1076@naver.com>
2021-04-21 23:48:49 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기 @Query, 값, DTO 조회하기 파라미터 바인딩 반환타입

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
@@ -16,4 +18,23 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     // 실무에서 거의 사용안함 -> 엔티티에 쿼리 쓰는게 이상함 , 다른 기능이 대체함
     // 가장 큰 장점 쿼리가 잘못된경우, application 로딩 시점에 오류를 감지
     List<Member> findByUsername(@Param("username") String username);
+
+    // 실제 실무에서 많이 사용
+    // @Query에서 오타가난경우 application loading 시점에서 에러 감지
+    @Query("select m from Member m where m.username =:username and m.age = :age")
+    List<Member> findUser( @Param("username") String username, @Param("age") int age);
+
+    // 단순 조회
+    @Query("select m.username from Member m")
+    List<String> findUserNameList();
+
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUsername(String username); // 컬렉션
+    Member findMemberByUsername(String username); // 단건
+    Optional<Member> findOptionalByUsername(String username);// 단건 Optional
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import javax.swing.text.html.Option;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @Test
     public void testMember(){
@@ -88,6 +94,92 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findByUsername("AAA");
         Member findMember = result.get(0);
         assertThat(findMember).isEqualTo(m1);
+
+    }
+
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("AAA",20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        assertThat(result.get(0)).isEqualTo(m1);
+
+    }
+
+    @Test
+    public void findUsernameList(){
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("BBB",20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> userNameList = memberRepository.findUserNameList();
+
+        assertThat(userNameList.contains("AAA")).isTrue();
+        assertThat(userNameList.contains("BBB")).isTrue();
+        assertThat(userNameList.contains("CCC")).isFalse();
+    }
+
+    @Test
+    public void findUsernameDto(){
+
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA",10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        assertThat(memberDto.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(memberDto.get(0).getTeamName() ).isEqualTo("teamA");
+
+    }
+
+    @Test
+    public void findByNames(){
+
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("BBB",20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+
+    }
+
+    @Test
+    public void returnType(){
+        Member m1 = new Member("AAA",10);
+        Member m2 = new Member("BBB",20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findListByUsername("asdfasdf");// 데이터가 없을때 null 이아님
+        System.out.println("result = " + result.size() );
+
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        Member findMember2 = memberRepository.findMemberByUsername("asfasdfasdf");
+        System.out.println("findMember2 = " + findMember2 );
+
+        Optional<Member> findMemberOptional = memberRepository.findOptionalByUsername("asdfasdf");
+        System.out.println("findMemberOptional = " + findMemberOptional );
+
+        // optional에서 단건조회일때, 결과가 단건이 아닌경우(=여러개인경우, Exception(=IncorrectResultSizeDataAccessException) 발생)
+        Member m3 = new Member("AAA",20);
+        memberRepository.save(m3);
+
+        findMemberOptional = memberRepository.findOptionalByUsername("AAA");
+
 
     }
 }
