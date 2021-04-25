d08a8c446fb632977db1d40ef34aee1af415de86
Tdear <wish1076@naver.com>
2021-04-25 22:12:03 +0900

    @EntityGraph

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -26,7 +27,7 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     // 실제 실무에서 많이 사용
     // @Query에서 오타가난경우 application loading 시점에서 에러 감지
     @Query("select m from Member m where m.username =:username and m.age = :age")
-    List<Member> findUser( @Param("username") String username, @Param("age") int age);
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
     // 단순 조회
     @Query("select m.username from Member m")
@@ -39,7 +40,9 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     List<Member> findByNames(@Param("names") List<String> names);
 
     List<Member> findListByUsername(String username); // 컬렉션
+
     Member findMemberByUsername(String username); // 단건
+
     Optional<Member> findOptionalByUsername(String username);// 단건 Optional
 
     // Page<Member> findByAge(int age, Pageable pageable);
@@ -53,4 +56,22 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Member m set m.age = m.age+1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+//    @EntityGraph(attributePaths = {"team"})
+    @EntityGraph("Member.all")
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
+
+
+
 }
